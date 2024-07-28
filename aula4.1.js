62e6c5bbe4bf9e0d127218994f29bc15a3c23a9d
lytazy <pedroviniciusdec@gmail.com>
2024-07-28 17:11:11 -0300

    Create aula4.1.js

---
+++ aula4.1.js
@@ -0,0 +1,23 @@
+//operadores relacionais vai ser utilizado para criar as condiç~oes
+//operadores logico serao utilizado para definir duas ou mais condiçoes para fazer essa junçao utilizamos as operadores logico
+//sao eles e, ou,nao
+
+//operador e
+//no java script o "e" sera && e ela sera so utilizada quando as duas conjunçoes forem verdadeira se tiver um e o outro nao entao nao sera verdadeira
+//exemplo eu so vou ir trabalhar se eu tiver legal e disposiçao ai sera verdadeiro
+//se caso eu nao tiver legal nem disposiçao eu nao vou ir trabalhar
+//se eu tiver legal e sem disposiçao eu nao vou ir trabalhar 
+//se eu nao estiver legal mas com disposiçao eu nao vou ir trabalhar
+
+//operador ou
+//no javaScript e descrito como ||
+//seria, eu so vou trabalhar se eu tiver legal ou com disposiçao
+//se eu nao tiver legal mas com disposiç~ao e valido porque e um ou outro
+//se eu nao tiver leggal nem com disposiç~ao entao e invalido
+
+//operador nao
+//utilizado como ! sinal de negaçao para negar algo sendo muito semelhante a != mas e coisas diferente por ! nega ja o != e diferente de algo
+//se uma coisa nao e verdadeira entao e falsa
+//se uma coisa nao e falsa entao e verdadeira
+
+
