d4c07bb1a5f1fc14bbedf8f36f7aacf8022b284f
lytazy <pedroviniciusdec@gmail.com>
2024-07-28 17:11:45 -0300

    Create aula4.js

---
+++ aula4.js
@@ -0,0 +1,24 @@
+//estrutura condicionais que  sao else...if(se nao) tipo voce so vai atravessar se nao estiver vindo o carro (problema logico)
+//sintaxe e como a linguegem de programa vai entender e executar
+//if em ingles e (se)uma condiçao for verdadeira sempre abrindo o bloco de chaves e fechando{}
+//execute esse bloco de codigo se for verdadeira
+//else em ingles e (senao) caso contrario
+//execute esse outro bloco se a primeira condiçao nao for comprida
+//exemplo resumido:se o numero nao for par (se) ele so pode ser impar (senao)
+
+
+//estrutura condicional composta seria if(situaç~ao for verdadeira){
+//execute esete bloco
+//} else if (seituaç~ao verdadeira) senao se podendo executar varios else if dependendo da situaçao
+//na composta ela nao precisa ter outra condiçao que seria o else ela pode ser
+
+//condicionais simples e se uma coisa nao for resta apenas a outra e a composta que pode ter varias situaçoes
+//para que uma estrutura condicional aconteça  nois temos que passar uma expressao e essa expressao vai ser validade pelo um operador relacional
+//operadores relacionais serao utilizado para criar as condiçoes pois um operador relacional sempre ira retornar um valor logico (ou verdadeiro ou falsa)
+//s~ao eles 
+//> maior que 
+//< menor que
+//>= maior ou igual
+//<= menor ou igual
+// = igual
+// != diferente
